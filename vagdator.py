446ea5a0c4d4a71f53e00cdea3581cf9136985d7
neomossb <neo.mossback@globala.se>
2024-11-08 14:02:07 +0100

    webcam

--- vagdator.py
+++ vagdator.py
@@ -47,25 +47,37 @@ if __name__ == "__main__":
         print(df.loc[x, 'temp_max'])
 
 
-        weather = df.loc[x, 'weather']
         temp_max = df.loc[x, 'temp_max']
+
+        serial_string = str(temp_max)
+
+        weather_next = df.loc[x+1, 'weather']
+
         if serial_on:
-            ser.write(temp_max.encode())
+            ser.write(serial_string.encode())
 
         time.sleep(5)
         ret, frame = cap.read()
 
+        new_width, new_height = 128, 96
+
+        img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # convert to black and white
+        img_blur = cv2.GaussianBlur(img_gray, (3, 3), 0) # gaussian blur for better edge detection
+        sobel_xy = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5) # edge detection
+
+        resized_image = cv2.resize(sobel_xy, (new_width, new_height), interpolation=cv2.INTER_AREA)
+
         if ret:
             # Set up the save path
-            if weather == 'drizzle':
+            if weather_next == 'drizzle':
                 save_dir = drizzle_path  # Folder where you want to save the image
-            if weather == 'fog':
+            if weather_next == 'fog':
                 save_dir = fog_path
-            if weather == 'rain':
+            if weather_next == 'rain':
                 save_dir = rain_path
-            if weather == 'snow':
+            if weather_next == 'snow':
                 save_dir = snow_path
-            if weather == 'sun':
+            if weather_next == 'sun':
                 save_dir = sun_path
             print(save_dir)
 
@@ -75,7 +87,7 @@ if __name__ == "__main__":
             print(save_path)
 
             # Save the image
-            cv2.imwrite(save_path, frame)
+            cv2.imwrite(save_path, resized_image)
             print(f"Image saved to {save_path}")
         else:
             print("Error: Could not capture an image.")
